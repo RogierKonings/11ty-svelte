85813c4ff79b00f0dc964ad28f57256f2d01d597
Rogier Konings <rogier.konings@randstadgroep.nl>
2023-10-01 23:20:03 +0200

    slots

---
+++ src/utils/utils.js
@@ -0,0 +1,84 @@
+
+import {
+  destroy_component,
+  detach,
+  insert,
+  is_function,
+  mount_component,
+  noop,
+  SvelteComponent,
+} from "svelte/internal";
+
+
+const createNodeFromString = (string) =>
+  new DOMParser().parseFromString(string, "text/html").body.firstElementChild;
+
+const createSlots = (slots) => {
+  const svelteSlots = {};
+
+  for (const slotName in slots) {
+    svelteSlots[slotName] = [createSlotFn(slots[slotName])];
+  }
+
+  function createSlotFn([ele, props = {}]) {
+    if (is_function(ele) && ele.prototype instanceof SvelteComponent) {
+      let component;
+      return function () {
+        return {
+          c: noop,
+          m(target, anchor) {
+            component = new ele({ target, props });
+            mount_component(component, target, anchor, null);
+          },
+          d(detaching) {
+            destroy_component(component, detaching);
+          },
+          l: noop,
+        };
+      };
+    } else {
+      return function () {
+        return {
+          c: noop,
+          m: function mount(target, anchor) {
+            insert(target, ele, anchor);
+          },
+          d: function destroy(detaching) {
+            if (detaching) {
+              detach(ele);
+            }
+          },
+          l: noop,
+        };
+      };
+    }
+  }
+  return svelteSlots;
+};
+
+const registerComponent = (component, name) => {
+  document.querySelectorAll(`.${CSS.escape(name)}`).forEach(($el) => {
+    const props = JSON.parse($el.dataset.props);
+    delete $el.dataset.props;
+    const content = createNodeFromString($el.dataset.content);
+    delete $el.dataset.content;
+
+    new component({
+      target: $el,
+      props: {
+        ...props,
+        ...(content
+          ? {
+              $$slots: createSlots({
+                default: [content],
+              }),
+              $$scope: {},
+            }
+          : {}),
+      },
+      hydrate: true,
+    });
+  });
+}
+
+export { createNodeFromString, registerComponent };
